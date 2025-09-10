b3ab2dbaf34fb8086541332110b8d8a772a53eaa
emkaybarrie <106494695+emkaybarrie@users.noreply.github.com>
2025-09-10 08:31:24 +0100

    Added Gina avatar

--- Project MyFi/js/dashboard.js
+++ Project MyFi/js/dashboard.js
@@ -211,7 +211,7 @@ document.addEventListener("DOMContentLoaded", () => {
       console.log("Player data ready:", playerData);
       let avatarKey = playerData.avatarKey || "default";
  
-      const avatarsSource = ["Azakai","Maestro","Richard","Mohammed","Umee","Amandeep","Matthew","Gerard","Sammi","Kirsty", "Lola", "Ted"];
+      const avatarsSource = ["Azakai","Maestro", "Lola", "Umee", "Plutus", "Gina" ,"Kirsty", "Richard","Mohammed","Amandeep","Matthew","Gerard","Sammi"];
 
      if (avatarKey === 'default' || avatarKey === '' || avatarsSource.includes(playerData.alias) || avatarsSource.includes(playerData.firstName)){
 
