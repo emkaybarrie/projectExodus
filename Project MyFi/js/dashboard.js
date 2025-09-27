377feda43b28417bc432e680b9b3b1e29b9964f5
emkaybarrie <106494695+emkaybarrie@users.noreply.github.com>
2025-09-27 15:44:00 +0100

    Smart review and TL to Review flow

--- Project MyFi/js/dashboard.js
+++ Project MyFi/js/dashboard.js
@@ -159,7 +159,7 @@ import { createRouter } from './navigation.js';
 // in dashboard.js (post onAuthStateChanged success)
 import { initQuestEngine } from "../quests/questEngine.js";
 import { httpsCallable } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-functions.js";
-import { showSmartReviewAfterTrueLayer } from './smartReview.js'
+import { maybeOpenSmartReviewOnLoad } from './smartReview.js'
 
 import "./core/truelayer.js";
 import "./modal.js";
@@ -277,7 +277,7 @@ document.addEventListener("DOMContentLoaded", () => {
       setTimeout(() => window.MyFiShowIOSInstallModal({ autoPulse: true }), 300);
     }
 
-    showSmartReviewAfterTrueLayer()
+    maybeOpenSmartReviewOnLoad()
 
     /* ---------- SLIDE ROUTER (Vitals hub + satellites) ---------- */
 
